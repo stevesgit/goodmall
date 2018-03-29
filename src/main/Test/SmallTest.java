84dcc09ea636b036626cb6e37f76f3e59a010bd3
stevesgit <wjh2013a@icloud.com>
2018-03-29 13:54:21 +0800

    基本完成待统测

--- src/main/Test/SmallTest.java
+++ src/main/Test/SmallTest.java
@@ -9,6 +9,11 @@ import java.util.*;
  * Created By SteveWoo
  */
 public class SmallTest implements Serializable {
+    private Integer number;
+
+    /**
+     * Guava 测试
+     */
     @Test
     public void test1() {
         String str = "a,b,c";
@@ -31,5 +36,36 @@ public class SmallTest implements Serializable {
         set.add("u");
         String str = Joiner.on(",").join(set);
         System.out.println(str);
+    }
+
+    /**
+     * 自动装箱拆箱注意点
+     */
+    @Test
+    public void test3() {
+        long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    //值比较 Integer 在-128 ~ 127内是相等的 jvm只会创建一个Integer对象 a和b引用同一份对象
+    //所以这里是true
+    @Test
+    public void test4() {
+        Integer a = 1;
+        Integer b = 1;
+        System.out.println(a == b);
+    }
+
+    @Test
+    public void test5() {
+        //NullPointerException on unboxing
+        //拆箱遇到空指针错误 因为这个原本为null
+        System.out.println(number);
+        if (number < 3) {
+            System.out.println("success");
+        }
     }
 }
--- src/main/java/com/mmall/controller/protal/CartController.java
+++ src/main/java/com/mmall/controller/protal/CartController.java
@@ -22,7 +22,12 @@ public class CartController {
     @Autowired
     private ICartService iCartService;
 
-
+    /**
+     * 列表
+     *
+     * @param session
+     * @return
+     */
     @RequestMapping("list.do")
     @ResponseBody
     public ServerResponse<CartVo> list(HttpSession session) {
@@ -33,6 +38,14 @@ public class CartController {
         return iCartService.list(user.getId());
     }
 
+    /**
+     * 添加产品
+     *
+     * @param session
+     * @param count
+     * @param productId
+     * @return
+     */
     @RequestMapping("add.do")
     @ResponseBody
     public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId) {
@@ -43,6 +56,13 @@ public class CartController {
         return iCartService.add(user.getId(), productId, count);
     }
 
+    /**
+     * 移除产品
+     *
+     * @param session
+     * @param productIds
+     * @return
+     */
     @RequestMapping("delete_product.do")
     @ResponseBody
     public ServerResponse<CartVo> deleteProduct(HttpSession session, String productIds) {
@@ -53,6 +73,12 @@ public class CartController {
         return iCartService.deleteProduct(user.getId(), productIds);
     }
 
+    /**
+     * 购物车全选
+     *
+     * @param session
+     * @return
+     */
     @RequestMapping("select_all.do")
     @ResponseBody
     public ServerResponse<CartVo> selectAll(HttpSession session) {
@@ -63,6 +89,12 @@ public class CartController {
         return iCartService.selectOrUnSelect(user.getId(), null, Const.Cart.CHECKED);
     }
 
+    /**
+     * 撤销全选
+     *
+     * @param session
+     * @return
+     */
     @RequestMapping("un_select_all.do")
     @ResponseBody
     public ServerResponse<CartVo> unSelectAll(HttpSession session) {
@@ -71,6 +103,56 @@ public class CartController {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
         }
         return iCartService.selectOrUnSelect(user.getId(), null, Const.Cart.UN_CHECKED);
+    }
+
+    /**
+     * 单选产品
+     *
+     * @param session
+     * @param productId
+     * @return
+     */
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse<CartVo> select(HttpSession session, Integer productId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.selectOrUnSelect(user.getId(), productId, Const.Cart.CHECKED);
+    }
+
+    /**
+     * 撤销单选产品
+     *
+     * @param session
+     * @param productId
+     * @return
+     */
+    @RequestMapping("un_select.do")
+    @ResponseBody
+    public ServerResponse<CartVo> unSelect(HttpSession session, Integer productId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.selectOrUnSelect(user.getId(), productId, Const.Cart.UN_CHECKED);
+    }
+
+    /**
+     * 获取购物车产品总数
+     *
+     * @param session
+     * @return
+     */
+    @RequestMapping("get_cart_product_count.do")
+    @ResponseBody
+    public ServerResponse<Integer> getCartProductCount(HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createBySuccess(0);
+        }
+        return iCartService.getCartProductCount(user.getId());
     }
 
 
